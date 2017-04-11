a662c26e5a25a69d180d7c8a5ea3899c36abc63b
Juan Juarez <juanjuarez150@juans-macbook-pro.local>
2017-04-11 12:12:04 -0700

    Added the third and fourth views

---
+++ ParkingHermit/FourthViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FourthViewController.swift
+//  ParkingHermit
+//
+//  Created by Juan Juarez on 4/10/17.
+//  Copyright © 2017 jdjuarez. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ ParkingHermit/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  ParkingHermit
+//
+//  Created by Juan Juarez on 4/10/17.
+//  Copyright © 2017 jdjuarez. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
