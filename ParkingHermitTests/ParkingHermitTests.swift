03d5e2845e96d96553ac8aa0a97d985b898b6f5d
Juan Juarez <juanjuarez150@juans-macbook-pro.local>
2017-04-10 19:39:17 -0700

    Initial Commit

---
+++ ParkingHermitTests/ParkingHermitTests.swift
@@ -0,0 +1,36 @@
+//
+//  ParkingHermitTests.swift
+//  ParkingHermitTests
+//
+//  Created by Juan Juarez on 4/10/17.
+//  Copyright © 2017 jdjuarez. All rights reserved.
+//
+
+import XCTest
+@testable import ParkingHermit
+
+class ParkingHermitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
