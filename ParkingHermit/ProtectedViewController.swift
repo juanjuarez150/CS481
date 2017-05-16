b0bd76de5bde1541c356e695989f35662d5d32d8
So Cal <bc@catcott.com>
2017-05-16 14:30:03 -0700

    comment

---
+++ ParkingHermit/ProtectedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProtectedViewController.swift
+//  ParkingHermit
+//
+//  Created by So Cal on 5/16/17.
+//  Copyright © 2017 jdjuarez. All rights reserved.
+//
+
+import UIKit
+
+class ProtectedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
