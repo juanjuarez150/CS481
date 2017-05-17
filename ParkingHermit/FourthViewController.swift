6256c3e860335663720996d74d2ae976f1a3c016
So Cal <bc@catcott.com>
2017-05-16 19:18:12 -0700

    Almost working Messaging

--- ParkingHermit/FourthViewController.swift
+++ ParkingHermit/FourthViewController.swift
@@ -7,9 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class FourthViewController: UIViewController {
-    
+class FourthViewController: UIViewController, MFMessageComposeViewControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +20,28 @@ class FourthViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBOutlet weak var txtMessage: UITextField!
+    @IBOutlet weak var contactTxt: UITextField!
+    @IBOutlet weak var txtLabel: UILabel!
+    
+    @IBAction func SendMessage(_ sender: Any) {
+        if MFMessageComposeViewController.canSendText() {
+            let control = MFMessageComposeViewController()
+            control.body = self.txtMessage.text
+            control.recipients = [self.contactTxt.text!]
+            self.present(control, animated: true, completion: nil)
+            
+ 
+        }
+      
+            let text1 = txtMessage.text
+            txtLabel.text = text1
+            txtMessage.text = ""
+    }
+
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        self.dismiss(animated: true, completion: nil)
+    }
     
 }
 
